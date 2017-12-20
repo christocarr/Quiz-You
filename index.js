8c74bbc5d6b2959aa11efb8e389e31009f29d7c7
Chris Carr <christocarr@outlook.com>
2017-12-20 20:10:14 +0000

    added some functionality

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-questionAnswer = {
-	
+let questionContainer = document.getElementById('question');
+
+let answer = document.getElementById('answer');
+
+let submit = document.getElementById('submit');
+
+const questions = ['questionOne', 'questionTwo', 'questionThree'];
+
+questionContainer.innerHTML = questions[2] + '?';
+
+console.log(answer.value);
+
+submit.onclick = () => { 
+	console.log('Clicked');
 }
