3fb4630765d322c85ebfc223b0c3229a08aa3173
Chris Carr <christocarr@outlook.com>
2017-12-22 17:59:44 +0000

    change of functionality and added question object

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 let questionContainer = document.getElementById('question');
 
-let answer = document.getElementById('answer');
+let choiceOne = document.getElementById('choiceOne');
+let choiceOneTwo = document.getElementById('choiceTwo');
+let choiceThree = document.getElementById('choiceThree');
 
 let submit = document.getElementById('submit');
 
-const questions = ['questionOne', 'questionTwo', 'questionThree'];
+const questions = { 
+	questionOne: { 
+		question : 'What is the capital of South Africa?', 
+		answers : ['Johannesburg', 'Cape Town', 'Pretoria'],
+		correctAnswer: 'Pretoria'
+	},
+	questionTwo: {
+		question: 'What is the longest river in USA?',
+		answers: ['Danube', 'Mississipi', 'Dakota'],
+		correctAnswer: 'Mississipi'
+	}
+}
+									 
+questionContainer.innerHTML = questions.questionOne.question;
 
-questionContainer.innerHTML = questions[2] + '?';
+choiceOne.innerHTML = questions.questionOne.answers[0];
+choiceTwo.innerHTML = questions.questionOne.answers[1];
+choiceThree.innerHTML = questions.questionOne.answers[2];
 
-console.log(answer.value);
+console.log(questionContainer);
 
 submit.onclick = () => { 
-	console.log('Clicked');
+	
 }
