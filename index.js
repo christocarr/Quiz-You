d78ba4cde8458cd104eeb95744ae154da9e84c80
Chris <christocarr@outlook.com>
2018-11-04 12:10:27 +0000

    Fix typos

--- index.js
+++ index.js
@@ -27,7 +27,7 @@
 		},
 		questionThree = {
 			question: 'What is an axolotl?',
-			answers: ['A species of salamander', 'A mulit-axel vehicle', 'A bone in the human foot'],
+			answers: ['A species of salamander', 'A multi-axel vehicle', 'A bone in the human foot'],
 			correctAnswer: 'A species of salamander'
 		},
 		questionFour = {
@@ -56,13 +56,13 @@
 		answerWrapper.style.display = 'block';
 		//hide instructions 
 		startScreen.style.display = 'none';
-		//dipslay first question
+		//display first question
 		let index = 0;
 		displayQuestion(index);
 	}
 
 	//enable next button on radio button click
-	function handleSelect(optionsArr) {
+	function handleNext(optionsArr) {
 		optionsArr.forEach(function(elem) {
 			elem.addEventListener('click', function(){
 				SUBMIT.disabled = false;
@@ -111,14 +111,27 @@
 		
 		const OPTIONS = document.getElementsByTagName('input');
 		optionsArr = [...OPTIONS];
-		handleSelect(optionsArr);
+		handleNext(optionsArr);
 	}
 
 	let nextQuestion = () => {
 		//get next question and answers in questions array
 		currentQuestion++;
 		let index = currentQuestion;
+
 		displayQuestion(index);
+		
+		//check if end of quiz
+		// if(currentQuestion <= QUESTIONS.length) {
+		// 	displayQuestion(index);
+		// 	console.log(currentQuestion);
+		// } else {
+		// 	displayScore()
+		// }
+	}
+
+	let displayScore = () => {
+		console.log(currentScore);
 	}
 
 })()
