b45af739fbb0ff689ffb16517d13ab83c88d6856
christocarr <christocarr@outlook.com>
2019-02-03 11:45:54 +0000

    Update activate event listner

--- sw.js
+++ sw.js
@@ -1,42 +1,40 @@
-let CACHE_NAME = 'cache-v1';
-const URLS_TO_CACHE = [
+const CACHE_NAME = 'cache-v2';
+const CACHE_ASSETS = [
   'index.html',
-  'styles.css',
-  'index.js',
-  'questions.js',
-  'manifest.json'
+  '/css/main.css',
+  '/scripts',
+  '/data',
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Opened cache');
-      return cache.addAll(URLS_TO_CACHE);
+self.addEventListener('install', ev => {
+  ev.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then(cache => {
+        cache.addAll(CACHE_ASSETS);
+      })
+      .then(()=> self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', ev => {
+  //remove unwanted caches
+  ev.waitUntil(
+    caches.keys().then(CACHE_NAME => {
+      return Promise.all(
+        CACHE_NAME.map(cache => {
+          if(cache != CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
     })
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-    .then((response) => {
-      if(response) {
-        return response;
-      }
-      return fetch(event.request)
-      .then((response) => {
-        if(!response || response.status !== 200 || response.type !== 'basic') {
-          return response;
-        }
-
-        let responseToCache = response.clone();
-
-        caches.open(CACHE_NAME)
-        .then((cache) => {
-          cache.put(event.request, responseToCache);
-        });
-        return response;
-      });
-    })
+self.addEventListener('fetch', ev => {
+  ev.respondWith(
+    fetch(ev.request).catch(() => caches.match(ev.request))
   );
 });
+
